2750781e41b416a897366948f1f7e8a417a03437
Shivank Kapur <shivankkapur2004@gmail.com>
2024-03-15 10:33:36 +0530

    started rust smart contract

--- programs/solpayflex/src/lib.rs
+++ programs/solpayflex/src/lib.rs
@@ -4,10 +4,22 @@ declare_id!("vjVYwt9ssMraEBQhbzfNr4bmdLvNN87inq4NSN9ai9J");
 
 #[program]
 pub mod solpayflex {
+    use anchor_lang::solana_program::entrypoint::ProgramResult;
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
+    #[account]
+    pub struct solpayflex {
+        pub buyer: Pubkey,
+        pub seller: Pubkey,
+        pub item: Pubkey,
+        pub amount: u64,
+        pub paid: u64, // amount paid at the moment
+        pub created_at: u64,
+    }
+
+    pub fn initialize(ctx: Context<Initialize>, amount: u64) -> ProgramResult {
+        let solpayflex = ctx.
     }
 }
 
